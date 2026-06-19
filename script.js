13b6e4669f574c0e8c70aa4102ba9014b4b8c40d
azka-ansari <azkaazkaansari7@gmail.com>
2026-06-20 03:53:19 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,117 @@
+// Question..1
+// var ch = prompt("Enter a character");
+
+// if (ch >= "0" ) {
+//     document.write("Number");
+// }
+// else if (ch >= "A" && ch <= "Z") {
+//     document.write("Uppercase Letter");
+// }
+// else if (ch >= "a" && ch <= "z") {
+//     document.write("Lowercase Letter");
+// }
+// else {
+//     document.write("Invalid Input");
+// }
+
+// Question..2
+
+// var num1 = 15;
+// var num2 = 20;
+
+
+// if (num1 > num2) {
+//     document.write(num1 + " bara hai");
+// }
+// else if (num2 > num1) {
+//     document.write(num2 + " bara hai");
+// }
+// else {
+//     document.write("Barabar hain");
+// }
+
+// Question..3
+// var num = prompt("Koi number likho:");
+// num = Number(num);
+
+// if (num > 0) {
+//     document.write(num + " positive hai");
+// }
+//  else if (num < 0) {
+//     document.write(num + " negative hai");
+// } 
+// else {
+//     document.write("Number zero hai");
+// }
+
+
+// Question..4
+// var user = prompt("enter a character");
+
+// var vowel = ["a", "e", "i", "o", "u"];
+
+// var result = false;
+
+// for (var i = 0; i < vowel.length; i++) {
+
+//     if (user == vowel[i]) {
+
+//         var result = true;
+
+//         document.write("ture" + "<br>");
+//     }
+
+// }
+
+// if (result==false) {
+//     document.write("False")
+// };
+
+// Question..5
+
+// var Password ="Azkaansari";
+// var userPassword=prompt("enter your Password")
+
+// if (userPassword =="") {
+//     document.write("Please enter your password");
+// }
+// else if (userPassword ==Password) {
+
+//         document.write("Correct");
+// }
+
+// else{
+//     document.write("Incorrect password");
+
+// };
+
+
+// Question..6
+
+// var hour =18;
+// var user= prompt("enter your hour")
+
+// if ( user < hour) {
+//     document.write( "Good day");
+// }else{
+//     document.write("Good evening");
+// };
+
+// Question..7
+
+// var time = prompt("Enter time in 24-hour format (e.g. 1900):");
+
+// if (time < 1200) {
+//     document.write("Morning");
+
+// } else if (time < 1600) {
+//     document.write("Afternoon")
+// }
+// else if (time < 2000) {
+//     document.write("Evening")
+// }
+
+// else{
+//     document.write("Invalid time")
+// }
+
